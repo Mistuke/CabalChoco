b902ca663f770cd081ddb70fad2d7e096f9c24b6
Tamar Christina <tamar@zhox.com>
2016-05-06 15:57:28 +0200

    Choco: fixed build script

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 $path = Split-Path $MyInvocation.MyCommand.Path -Parent
 $bin  = Join-Path $path "bin"
 Write-Host "$bin"
-ls ghc*.nuspec -recurse -File | ForEach-Object { 
+ls cabal*.nuspec -recurse -File | ForEach-Object { 
             $dir = (Split-Path $_.FullName -Parent)
             Write-Host "Compiling $_"
             cd $dir
