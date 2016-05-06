9458659e4c38d2b7e7f475410edf00cb78fa28e6
Tamar Christina <tamar@zhox.com>
2016-05-06 15:53:39 +0200

    Choco: Add initial import.

---
+++ templates/chocolateyInstall-Template.ps1
@@ -0,0 +1,21 @@
+﻿try {
+  #$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  ### For BinRoot, use the following instead ###
+  $binRoot         = Get-BinRoot
+  $installDir      = Join-Path $binRoot "$packageName"
+  $packageFullName = Join-Path $installDir ($packageName + '-' + $version)
+  $binPackageDir   = Join-Path $packageFullName "bin"
+  
+  # if removing $url64, please remove from here
+  # despite the name "Install-ChocolateyZipPackage" this also works with 7z archives
+  Install-ChocolateyZipPackage "$packageName" "$url" (Join-Path $env:chocolateyPackageFolder tmp) "$url64"
+  Get-ChocolateyUnzip -fileFullPath (Join-Path $env:chocolateyPackageFolder (Join-Path tmp ($packageName + "Install"))) -destination "$installDir"
+
+  Write-Host "Adding `'$binPackageDir`' to the path and the current shell path"
+  Install-ChocolateyPath "$binPackageDir"
+  # Reload the session to update path
+  Write-Host "$packageName $version has been installed. Before you can use it restart the console"
+} catch {
+  throw $_.Exception
+}
+
---
+++ templates/chocolateyUninstall-Template.ps1
@@ -0,0 +1,20 @@
+﻿try {
+  #$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  ### For BinRoot, use the following instead ###
+  $binRoot         = Get-BinRoot
+  $installDir      = Join-Path $binRoot "$packageName"
+  $packageFullName = Join-Path $installDir ($packageName + '-' + $version)
+  $binPackageDir   = Join-Path $packageFullName "bin"
+  $zipPackage      = Join-Path $env:chocolateyPackageFolder (Join-Path tmp ($packageName + "Install"))
+  
+  # first uninstal the package
+  UnInstall-ChocolateyZipPackage "$packageName" "$zipPackage"
+  # then clean up the directory
+  # just blast away everything since the folders are versioned, and if we got this far the package was installed by us
+  rm -r -fo $packageFullName
+
+  Write-Host "Removing `'$binPackageDir`' from the path and the current shell path"
+  UnInstall-Path "$binPackageDir"
+} catch {
+  throw $_.Exception
+}
